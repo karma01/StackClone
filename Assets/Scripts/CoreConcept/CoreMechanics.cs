92f9baca98164f408572e5e25bc0ba179b3f1b72
karma <ps716573@gmail.com>
2023-10-03 21:33:22 +0545

    Added touch system

--- Assets/Scripts/CoreConcept/CoreMechanics.cs
+++ Assets/Scripts/CoreConcept/CoreMechanics.cs
@@ -1,7 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+using touched = UnityEngine.InputSystem.EnhancedTouch.Touch;
+using TouchPhase = UnityEngine.InputSystem.TouchPhase;
 
 public class CoreMechanics : MonoBehaviour
 {
     private GameObject tileObject;
-    
+
+    private void Update()
+    {
+        IndentifyTouch();
+    }
+
+    private void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+    }
+
+    private void OnDisable()
+    {
+        EnhancedTouchSupport.Disable();
+    }
+
+    /// <summary>
+    /// Identify if the player has touched the screen
+    /// </summary>
+    private void IndentifyTouch()
+    {
+        bool isTouched = touched.fingers[0].isActive;
+        if (isTouched)
+        {
+            touched myTouch = touched.activeTouches[0];
+            if (myTouch.phase == TouchPhase.Began)
+
+            {
+                Debug.Log("IS Touched");
+            }
+        }
+    }
 }
